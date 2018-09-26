14c05d46789df06c4338a35c281b68596c541923
mattertater <mmcdade9@gmail.com>
2018-09-26 18:17:03 -0400

    fixed anm hw2

--- anm/hw2/secant.m
+++ anm/hw2/secant.m
@@ -3,10 +3,9 @@ cv = zeros(1, maxiter);
 cv(1) = x0;
 cv(2) = x1;
 for k=2:maxiter
-    cv(k + 1) = cv(k) - (f(cv(k)) / (f(cv(k)) - f(cv(k-1)) / cv(k) - cv(k-1)));
+    cv(k + 1) = cv(k) - ((f(cv(k)) * (cv(k) - (cv(k-1)))) / (f(cv(k)) - f(cv(k-1))));
     abserr = abs(cv(k+1) - cv(k));
     relerr = abserr / (abs(cv(k)) + eps);
-
     if (abserr < tol) && (relerr < tol)
         cv(maxiter) = cv(k+1);
         break;
