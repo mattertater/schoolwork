07209a1e17f4884143e54f2ba89b735cd6bee92d
mattertater <mmcdade9@gmail.com>
2018-11-07 17:28:25 -0500

    algo project 2 done

--- algo/project 2/proj2_test_suite/test.bat
+++ algo/project 2/proj2_test_suite/test.bat
@@ -1,77 +1,77 @@
 @echo off
 >output.txt (
 
-hull.exe Graham circle10.txt
-hull.exe Graham circle1000.txt
-hull.exe Graham circle10000.txt
-hull.exe Graham circle100000.txt
-hull.exe Graham circle1000000.txt
+a.exe G circle10.txt
+a.exe G circle1000.txt
+a.exe G circle10000.txt
+a.exe G circle100000.txt
+a.exe G circle1000000.txt
 
-hull.exe Jarvis circle10.txt
-hull.exe Jarvis circle1000.txt
-hull.exe Jarvis circle10000.txt
-hull.exe Jarvis circle100000.txt
-hull.exe Jarvis circle1000000.txt
+a.exe J circle10.txt
+a.exe J circle1000.txt
+a.exe J circle10000.txt
+a.exe J circle100000.txt
+a.exe J circle1000000.txt
 
-hull.exe Quick circle10.txt
-hull.exe Quick circle1000.txt
-hull.exe Quick circle10000.txt
-hull.exe Quick circle100000.txt
-hull.exe Quick circle1000000.txt
+a.exe Q circle10.txt
+a.exe Q circle1000.txt
+a.exe Q circle10000.txt
+a.exe Q circle100000.txt
+a.exe Q circle1000000.txt
 
-hull.exe Graham onCircle10.txt
-hull.exe Graham onCircle1000.txt
-hull.exe Graham onCircle10000.txt
-hull.exe Graham onCircle100000.txt
-hull.exe Graham onCircle1000000.txt
+a.exe G onCircle10.txt
+a.exe G onCircle1000.txt
+a.exe G onCircle10000.txt
+a.exe G onCircle100000.txt
+a.exe G onCircle1000000.txt
 
-hull.exe Jarvis onCircle10.txt
-hull.exe Jarvis onCircle1000.txt
-hull.exe Jarvis onCircle10000.txt
-hull.exe Jarvis onCircle100000.txt
-hull.exe Jarvis onCircle1000000.txt
+a.exe J onCircle10.txt
+a.exe J onCircle1000.txt
+a.exe J onCircle10000.txt
+a.exe J onCircle100000.txt
+a.exe J onCircle1000000.txt
 
-hull.exe Quick onCircle10.txt
-hull.exe Quick onCircle1000.txt
-hull.exe Quick onCircle10000.txt
-hull.exe Quick onCircle100000.txt
-hull.exe Quick onCircle1000000.txt
+a.exe Q onCircle10.txt
+a.exe Q onCircle1000.txt
+a.exe Q onCircle10000.txt
+a.exe Q onCircle100000.txt
+a.exe Q onCircle1000000.txt
 
-hull.exe Graham triangle10.txt
-hull.exe Graham triangle1000.txt
-hull.exe Graham triangle10000.txt
-hull.exe Graham triangle100000.txt
-hull.exe Graham triangle1000000.txt
+a.exe G triangle10.txt
+a.exe G triangle1000.txt
+a.exe G triangle10000.txt
+a.exe G triangle100000.txt
+a.exe G triangle1000000.txt
 
-hull.exe Jarvis triangle10.txt
-hull.exe Jarvis triangle1000.txt
-hull.exe Jarvis triangle10000.txt
-hull.exe Jarvis triangle100000.txt
-hull.exe Jarvis triangle1000000.txt
+a.exe J triangle10.txt
+a.exe J triangle1000.txt
+a.exe J triangle10000.txt
+a.exe J triangle100000.txt
+a.exe J triangle1000000.txt
 
-hull.exe Quick triangle10.txt
-hull.exe Quick triangle1000.txt
-hull.exe Quick triangle10000.txt
-hull.exe Quick triangle100000.txt
-hull.exe Quick triangle1000000.txt
+a.exe Q triangle10.txt
+a.exe Q triangle1000.txt
+a.exe Q triangle10000.txt
+a.exe Q triangle100000.txt
+a.exe Q triangle1000000.txt
 
-hull.exe Graham rectangle10.txt
-hull.exe Graham rectangle1000.txt
-hull.exe Graham rectangle10000.txt
-hull.exe Graham rectangle100000.txt
-hull.exe Graham rectangle1000000.txt
+a.exe G rectangle10.txt
+a.exe G rectangle1000.txt
+a.exe G rectangle10000.txt
+a.exe G rectangle100000.txt
+a.exe G rectangle1000000.txt
 
-hull.exe Jarvis rectangle10.txt
-hull.exe Jarvis rectangle1000.txt
-hull.exe Jarvis rectangle10000.txt
-hull.exe Jarvis rectangle100000.txt
-hull.exe Jarvis rectangle1000000.txt
+a.exe J rectangle10.txt
+a.exe J rectangle1000.txt
+a.exe J rectangle10000.txt
+a.exe J rectangle100000.txt
+a.exe J rectangle1000000.txt
 
-hull.exe Quick rectangle10.txt
-hull.exe Quick rectangle1000.txt
-hull.exe Quick rectangle10000.txt
-hull.exe Quick rectangle100000.txt
-hull.exe Quick rectangle1000000.txt
+a.exe Q rectangle10.txt
+a.exe Q rectangle1000.txt
+a.exe Q rectangle10000.txt
+a.exe Q rectangle100000.txt
+a.exe Q rectangle1000000.txt
 
 )
 pause
