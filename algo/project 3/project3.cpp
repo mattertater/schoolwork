81536cc8b924df0bf07892238977bb37e23734fc
mattertater <mmcdade9@gmail.com>
2018-11-21 00:14:46 -0500

    added ppm support, reogranized test files

--- algo/project 3/project3.cpp
+++ algo/project 3/project3.cpp
@@ -13,25 +13,52 @@
 #include <ctime>
 #include <bits/stdc++.h>
 
-std::vector<std::vector<int> > createVector(int width, int height) {
-    std::vector<std::vector<int> > a;
-    a.resize(height);
+struct Pixel {
+    int R, G, B;
+};
+
+// Size the matrix correctly based on input image width and height
+void populateVector(std::vector<std::vector<int> > &matrix, int width, int height) {
+    matrix.resize(height);
     for (int i = 0; i < height; i++) {
-        a[i].resize(width);
-    } 
-    return a;
+        matrix[i].resize(width);
+    }
 }
 
+// Overload to handle Pixel mamtrices
+void populateVector(std::vector<std::vector<Pixel> > &matrix, int width, int height) {
+    matrix.resize(height);
+    for (int i = 0; i < height; i++) {
+        matrix[i].resize(width);
+    }
+}
+
+// Transpose the matrix
 std::vector<std::vector<int> > transpose(std::vector<std::vector<int> > &matrix) {
     int width = matrix[0].size();
     int height = matrix.size();
-    std::vector<std::vector<int> > transposedMatrix = createVector(height, width);
+    std::vector<std::vector<int> > transposedMatrix;
+    populateVector(transposedMatrix, height, width);
     for (int i = 0; i < height; i++)
         for (int j = 0; j < width; j++)
             transposedMatrix[j][i] = matrix[i][j];
     return transposedMatrix;
 }
 
+// Overload to handle Pixel matrices
+std::vector<std::vector<Pixel> > transpose(std::vector<std::vector<Pixel> > &matrix) {
+    int width = matrix[0].size();
+    int height = matrix.size();
+    std::vector<std::vector<Pixel> > transposedMatrix;
+    populateVector(transposedMatrix, height, width);
+    for (int i = 0; i < height; i++)
+        for (int j = 0; j < width; j++)
+            transposedMatrix[j][i] = matrix[i][j];
+    return transposedMatrix;
+}
+
+// Print the matrix out for quick debugging
+// Careful when using with large inputs
 void printMatrix(std::vector<std::vector<int> > &matrix, std::string name) {
     int height = matrix.size();
     int width = matrix[0].size();
@@ -73,7 +100,7 @@ std::pair<int, int> minCoodsFromAbove(std::vector<std::vector<int> > &cumulative
     if (j != cumulative[i].size() - 1)
         if (cumulative[i-1][j+1] < cumulative[min.first][min.second])
             min.second = j + 1;
-        
+
     return min;
 }
 
@@ -93,7 +120,8 @@ std::pair<int, int> minCoords(std::vector<std::vector<int> > &cumulative, int i)
 std::vector<std::vector<int> > calculateEnergyMatrix(std::vector<std::vector<int> > &matrix) {
     int height = matrix.size();
     int width = matrix[0].size();
-    std::vector<std::vector<int> > energyMatrix = createVector(width, height);
+    std::vector<std::vector<int> > energyMatrix;
+    populateVector(energyMatrix, width, height);
     int left, right, top, bottom;
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
@@ -116,13 +144,49 @@ std::vector<std::vector<int> > calculateEnergyMatrix(std::vector<std::vector<int
     return energyMatrix;
 }
 
+// Calculate the energy matrix for Pixel matrix
+std::vector<std::vector<int> > calculateEnergyMatrix(std::vector<std::vector<Pixel> > &matrix) {
+    int height = matrix.size();
+    int width = matrix[0].size();
+    std::vector<std::vector<int> > energyMatrix;
+    populateVector(energyMatrix, width, height);
+    int left, right, top, bottom;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0) left = 0; 
+            else left = abs(matrix[i][j].R - matrix[i][j-1].R) + 
+                        abs(matrix[i][j].G - matrix[i][j-1].G) + 
+                        abs(matrix[i][j].B - matrix[i][j-1].B);
+
+            if (j == width - 1) right = 0;
+            else right = abs(matrix[i][j].R - matrix[i][j+1].R) + 
+                         abs(matrix[i][j].G - matrix[i][j+1].G) +
+                         abs(matrix[i][j].B - matrix[i][j+1].B);
+
+            if (i == 0) top = 0;
+            else top = abs(matrix[i][j].R - matrix[i-1][j].R) + 
+                       abs(matrix[i][j].G - matrix[i-1][j].G) +
+                       abs(matrix[i][j].B - matrix[i-1][j].B);
+
+            if (i == height - 1) bottom = 0;
+            else bottom = abs(matrix[i][j].R - matrix[i+1][j].R) + 
+                          abs(matrix[i][j].G - matrix[i+1][j].G) +
+                          abs(matrix[i][j].B - matrix[i+1][j].B);
+
+            energyMatrix[i][j] = left + right + top + bottom;
+        }
+    }
+
+    return energyMatrix;
+}
+
 // Calculate cumulative matrix
-// TODO: investigate time sink, make it faster
 std::vector<std::vector<int> > calculateCumulativeMatrix(std::vector<std::vector<int> > &energyMatrix) {
 
     int height = energyMatrix.size();
     int width = energyMatrix[0].size();
-    std::vector<std::vector<int> > cumulativeMatrix = createVector(width, height);
+    std::vector<std::vector<int> > cumulativeMatrix;
+    populateVector(cumulativeMatrix, width, height);
 
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
@@ -153,10 +217,15 @@ std::vector<std::pair<int, int> > findMinPath(std::vector<std::vector<int> > &cu
 void removeMinPath(std::vector<std::vector<int> > &matrix, std::vector<std::pair<int, int> > &minPath) {
     for (int i = 0; i < minPath.size(); i++)
         matrix[minPath[i].first].erase(matrix[minPath[i].first].begin() + minPath[i].second);
-
+}
+// Overload for Pixel matrices
+void removeMinPath(std::vector<std::vector<Pixel> > &matrix, std::vector<std::pair<int, int> > &minPath) {
+    for (int i = 0; i < minPath.size(); i++)
+        matrix[minPath[i].first].erase(matrix[minPath[i].first].begin() + minPath[i].second);
 }
 
-std::vector<std::vector<int> > removeSeams(std::vector<std::vector<int> > &matrix, int seams) {
+// Main function to consolidate seam-removal functions
+void removeSeams(std::vector<std::vector<int> > &matrix, int seams) {
     std::vector<std::vector<int> > energyMatrix, cumulativeMatrix;
     std::vector<std::pair<int, int> > minPath;
 
@@ -167,19 +236,30 @@ std::vector<std::vector<int> > removeSeams(std::vector<std::vector<int> > &matri
         minPath = findMinPath(cumulativeMatrix);
         removeMinPath(matrix, minPath);
     }
+}
+// Overload for Pixel matrices
+void removeSeams(std::vector<std::vector<Pixel> > &matrix, int seams) {
+    std::vector<std::vector<int> > energyMatrix, cumulativeMatrix;
+    std::vector<std::pair<int, int> > minPath;
 
-    return matrix;
+    // remove "seams" amount of seams 
+    for (int count = 0; count < seams; count++) {
+        energyMatrix = calculateEnergyMatrix(matrix);
+        cumulativeMatrix = calculateCumulativeMatrix(energyMatrix);
+        minPath = findMinPath(cumulativeMatrix);
+        removeMinPath(matrix, minPath);
+    }
 }
 
 int main(int argc, char*argv[]) {
 
-
     if (argc < 4) {
-        std::cout << "Incorrect format: should be \"a.exe [image.pgm] [vertical seams] [horizontal seams]\"" << std::endl;
+        std::cout << "Incorrect format: should be \"a.exe [image.pgm/image.ppm] [vertical seams] [horizontal seams]\"" << std::endl;
     }
     else {
         int width = 0, height = 0, maxVal = 0;
         std::vector<std::vector<int> > pgm;
+        std::vector<std::vector<Pixel> > ppm;
         std::string p_value;
 
         // Store arguments in variables
@@ -194,7 +274,13 @@ int main(int argc, char*argv[]) {
         verticalInt >> vertical;
         horizontalInt >> horizontal;
 
-        std::string outputFilename = imageFilename.substr(0, imageFilename.size()-4) + "_processed.pgm";
+        std::string extension = imageFilename.substr(imageFilename.size()-4, imageFilename.size());
+        std::string outputFilename;
+
+        if (extension == ".pgm")
+            outputFilename = imageFilename.substr(0, imageFilename.size()-4) + "_processed.pgm";
+        else 
+            outputFilename = imageFilename.substr(0, imageFilename.size()-4) + "_processed.ppm";
 
         std::ifstream file;
         file.open(imageFilename.c_str());
@@ -218,14 +304,37 @@ int main(int argc, char*argv[]) {
                 else myChunk >> maxVal;
             }
 
-            // make 2d array of size gotten from top of pgm file
-            pgm = createVector(width, height);
-            // fill the array
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    file >> chunk;
-                    std::stringstream myVal(chunk);
-                    myVal >> pgm[i][j];
+            if (p_value == "pgm") {
+                // make 2d array of size gotten from top of pgm file
+                populateVector(pgm, width, height);
+                // fill the array
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        file >> chunk;
+                        std::stringstream myVal(chunk);
+                        myVal >> pgm[i][j];
+                    }
+                }
+            } else {
+                populateVector(ppm, width, height);
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        Pixel thisPixel;
+
+                        file >> chunk;
+                        std::stringstream myR(chunk);
+                        myR >> thisPixel.R;
+
+                        file >> chunk;
+                        std::stringstream myG(chunk);
+                        myG >> thisPixel.G;
+
+                        file >> chunk;
+                        std::stringstream myB(chunk);
+                        myB >> thisPixel.B;
+
+                        ppm[i][j] = thisPixel;
+                    }
                 }
             }
 
@@ -236,42 +345,60 @@ int main(int argc, char*argv[]) {
 
         if (maxVal != 0) {
             file.close();
-            // set pgm to the output of removing all the vertical seams
-            pgm = removeSeams(pgm, vertical);
 
-            // transpose the matrix so we can use the same function to remove horizontal seams
-            pgm = transpose(pgm);
+            if (p_value == "pgm") {
+                // set pgm to the output of removing all the vertical seams
+                removeSeams(pgm, vertical);
 
-            // set pgm to the output of removing all the horizontal seams
-            pgm = removeSeams(pgm, horizontal);
+                // transpose the matrix so we can use the same function to remove horizontal seams
+                pgm = transpose(pgm);
 
-            // transpose back to get the original orientation
-            pgm = transpose(pgm);
+                // set pgm to the output of removing all the horizontal seams
+                removeSeams(pgm, horizontal);
 
-            // pgm is now the trimmed image, output to file with new dimensions
-            std::ofstream outfile;
-            outfile.open(outputFilename.c_str(), std::ofstream::out | std::ofstream::trunc);
+                // transpose back to get the original orientation
+                pgm = transpose(pgm);
 
-            // export 4 initial entries
-            outfile << p_value << "\n";
-            outfile << "# Output after removing " << vertical << " vertical and " << horizontal << " seams from " << imageFilename << "\n";
-            outfile << pgm[0].size() << " " << pgm.size() << "\n";
-            outfile << maxVal << "\n";
+                // pgm is now the trimmed image, output to file with new dimensions
+                std::ofstream outfile;
+                outfile.open(outputFilename.c_str(), std::ofstream::out | std::ofstream::trunc);
 
-            // export the matrix
-            for (int i = 0; i < pgm.size(); i++) {
-                for (int j = 0; j < pgm[0].size(); j++) {
-                    outfile << pgm[i][j] << " ";
+                // export 4 initial entries
+                outfile << p_value << "\n";
+                outfile << "# Output after removing " << vertical << " vertical and " << horizontal << " seams from " << imageFilename << "\n";
+                outfile << pgm[0].size() << " " << pgm.size() << "\n";
+                outfile << maxVal << "\n";
+
+                // export the matrix
+                for (int i = 0; i < pgm.size(); i++) {
+                    for (int j = 0; j < pgm[0].size(); j++) {
+                        outfile << pgm[i][j] << " ";
+                    }
+                    outfile << "\n";
                 }
-                outfile << "\n";
+                outfile.close();
+            } else {
+                removeSeams(ppm, vertical);
+                ppm = transpose(ppm);
+                removeSeams(ppm, horizontal);
+                ppm = transpose(ppm);
+
+                std::ofstream outfile;
+                outfile.open(outputFilename.c_str(), std::ofstream::out | std::ofstream::trunc);
+
+                outfile << p_value << "\n";
+                outfile << "# Output after removing " << vertical << " vertical and " << horizontal << " seams from " << imageFilename << "\n";
+                outfile << ppm[0].size() << " " << ppm.size() << "\n";
+                outfile << maxVal << "\n";
+                for (int i = 0; i < ppm.size(); i++) {
+                    for (int j = 0; j < ppm[0].size(); j++) {
+                        outfile << ppm[i][j].R << " " << ppm[i][j].G << " " << ppm[i][j].B << " ";
+                    }
+                    outfile << "\n";
+                }
+                outfile.close();
             }
-
-            outfile.close();
-
             std::cout << "Output file exported to " << outputFilename << std::endl;
         }
-
-
-
     }
 }
